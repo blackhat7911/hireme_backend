7710b4d2cdb607631576865536ad73dff06d0943
Bishal Rana Magar <49014427+blackhat7911@users.noreply.github.com>
2021-09-25 21:38:22 +0545

    settings updated 3.o

--- hireme_backend/settings.py
+++ hireme_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = "django-insecure-#ik_n(cwz)vryga@47mnzzbdiju62i@ft6c#*22s&e$578))ox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', str(env("ALLOWED_HOST")), '*']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '*']
 
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 # Application definition
